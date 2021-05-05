aec542d1f3f8d4d2be28c97ea89349f08bbd2c57
Matheus <matheus_epitacio@gmail.com>
2021-05-05 00:05:13 -0300

    End Of Section 3

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Entities;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options){
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ Entities/AppUser.cs
@@ -0,0 +1,12 @@
+namespace DatingApp.API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        
+        
+
+    }
+}
